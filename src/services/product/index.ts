cecde8fe3f8d40529ac8642d7b7f58d2a7d89ac0
Your Name <phamtienquan2001@gmail.com>
2023-08-30 01:52:47 +0700

    update api

--- src/services/product/index.ts
+++ src/services/product/index.ts
@@ -1,6 +1,6 @@
 export const Product = {
   async getProducts() {
-    return await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/api/products`)
+    return await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/api/v1/products`)
       .then((res) => res.json())
       .then((d) => JSON.parse(d))
   }
